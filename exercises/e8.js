b28dc096641eaa9ab4935a857a321154a2a4630c
Justin Armstrong <armstrongjustin034@gmail.com>
2024-06-11 18:54:41 -0700

    passed test-8

--- exercises/e8.js
+++ exercises/e8.js
@@ -6,8 +6,37 @@
 
 export function getClientWithGreatestBalance(array) {
   // Your code goes here...
+  if (array.length === 0) {
+    return []; // Return an empty array if the input array is empty
+}
+
+// Sort the array based on balance in descending order
+array.sort((a, b) => b.balance - a.balance);
+
+// Return an array containing the first element (account with the greatest balance)
+return [array[0]];
 
 }
+
+export const bankAccounts = [
+  {
+    id: 1,
+    name: "Susan",
+    balance: 100.32,
+    deposits: [150, 30, 221],
+    withdrawals: [110, 70.68, 120],
+  },
+  { id: 2, name: "Morgan", balance: 1100.0, deposits: [1100] },
+  {
+    id: 3,
+    name: "Joshua",
+    balance: 18456.57,
+    deposits: [4000, 5000, 6000, 9200, 256.57],
+    withdrawals: [1500, 1400, 1500, 1500],
+  },
+  { id: 4, name: "Candy", balance: 0.0 },
+  { id: 5, name: "Phil", balance: 18, deposits: [100, 18], withdrawals: [100] },
+];
 
 
 
