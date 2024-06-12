d214c982b33bbe8ea7360d42e8cf311c66fbcce3
Justin Armstrong <armstrongjustin034@gmail.com>
2024-06-11 19:38:14 -0700

    passed test-16

--- exercises/e16.js
+++ exercises/e16.js
@@ -7,7 +7,17 @@
 
 export function flatArrays(array) {
   // Your code goes here...
-
+  const flattenedArray = [];
+  for (let element of array) {
+      if (Array.isArray(element)) {
+          for (let subElement of element) {
+              flattenedArray.push(subElement);
+          }
+      } else {
+          flattenedArray.push(element);
+      }
+  }
+  return flattenedArray;
 }
 
 
